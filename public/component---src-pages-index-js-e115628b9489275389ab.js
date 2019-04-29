1e388108e8bc514e832918e891a34d90e85c0c33
João Pedro Schmitz <jpedroschmitz@hotmail.com>
2019-04-29 14:01:16 -0300

    Remove public folder

--- public/component---src-pages-index-js-e115628b9489275389ab.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{140:function(n,e,o){"use strict";o.r(e);var t=o(0),l=o.n(t);e.default=function(){return l.a.createElement("div",null,"Hello world!")}}}]);
-//# sourceMappingURL=component---src-pages-index-js-e115628b9489275389ab.js.map
--- public/pages-manifest-568c09b5fbe337d1bce2.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{141:function(n){n.exports={pages:[{componentChunkName:"component---src-pages-index-js",jsonName:"index",path:"/"}],dataPaths:{index:"140/path---index-6a9-0SUcWyAf8ecbYDsMhQkEfPzV8"}}}}]);
-//# sourceMappingURL=pages-manifest-568c09b5fbe337d1bce2.js.map
