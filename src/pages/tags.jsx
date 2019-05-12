82534241f4b171856de0e210e21b716f64b5aa81
João Pedro Schmitz <jpedroschmitz@hotmail.com>
2019-05-12 15:31:52 -0300

    Finalização da página

--- src/pages/tags.jsx
+++ src/pages/tags.jsx
@@ -1,21 +1,43 @@
 import React from "react";
+import { graphql } from "gatsby";
 import Layout from "../components/Layout";
 import PageBar from "../components/Internal/PageBar";
 import Post from "../components/Internal/Post";
 import Container from "../components/UI/Grid/Container";
 import Row from "../components/UI/Grid/Row";
 
-export default () => (
+const TagsPage = ({
+  data: {
+    allMarkdownRemark: { group },
+  },
+}) => (
   <Layout>
-    <PageBar title="Tags" introduction="Veja todas as tags do blog" />
+    <PageBar title="Tags" introduction="Veja todas as tags do blog"/>
     <Container>
       <Row centered>
-        <Post title="Why you should start coding today, not tomorrow" quantity="5" />
-        <Post title="Why you should start coding today, not tomorrow" quantity="5" />
-        <Post title="Why you should start coding today, not tomorrow" quantity="5" />
-        <Post title="Why you should start coding today, not tomorrow" quantity="5" />
-        <Post title="Why you should start coding today, not tomorrow" quantity="5" />
+        {group.map(item => (
+          <Post
+            key={item.fieldValue}
+            title={item.fieldValue}
+            link="tag"
+            quantity={item.totalCount}
+            slug={item.fieldValue}
+          />
+        ))}
       </Row>
     </Container>
   </Layout>
 );
+
+export default TagsPage;
+
+export const pageQuery = graphql`
+  query {
+    allMarkdownRemark(limit: 2000) {
+      group(field: frontmatter___tags) {
+        fieldValue
+        totalCount
+      }
+    }
+  }
+`;
--- src/templates/tag.jsx
+++ src/templates/tag.jsx
@@ -1,21 +1,55 @@
 import React from "react";
+import { graphql } from "gatsby";
 import Layout from "../components/Layout";
 import PageBar from "../components/Internal/PageBar";
 import Post from "../components/Internal/Post";
 import Container from "../components/UI/Grid/Container";
 import Row from "../components/UI/Grid/Row";
 
-export default () => (
-  <Layout>
-    <PageBar title="Todos os posts com a tag Y" introduction="" />
-    <Container>
-      <Row centered>
-        <Post title="Why you should start coding today, not tomorrow" quantity="5" />
-        <Post title="Why you should start coding today, not tomorrow" quantity="5" />
-        <Post title="Why you should start coding today, not tomorrow" quantity="5" />
-        <Post title="Why you should start coding today, not tomorrow" quantity="5" />
-        <Post title="Why you should start coding today, not tomorrow" quantity="5" />
-      </Row>
-    </Container>
-  </Layout>
-);
+export default ({ pageContext, data }) => {
+  const { tag } = pageContext;
+  const { edges } = data.allMarkdownRemark;
+  return (
+    <Layout>
+      <PageBar title={`Tag: ${tag}`} introduction="" />
+      <Container>
+        <Row centered>
+          {edges.map(item => (
+            <Post
+              category={item.node.frontmatter.category}
+              date={item.node.frontmatter.date}
+              title={item.node.frontmatter.title}
+              slug={item.node.frontmatter.slug}
+              borderColor={item.node.frontmatter.color}
+            />
+          ))}
+        </Row>
+      </Container>
+    </Layout>
+  );
+};
+
+export const pageQuery = graphql`
+  query TagPage($tag: String) {
+    allMarkdownRemark(
+      limit: 1000
+      sort: { fields: [fields___prefix], order: DESC }
+      filter: { frontmatter: { tags: { in: [$tag] }, draft: { ne: true } } }
+    ) {
+      totalCount
+      edges {
+        node {
+          excerpt
+          frontmatter {
+            title
+            tags
+            date
+            slug
+            category
+            description
+          }
+        }
+      }
+    }
+  }
+`;
