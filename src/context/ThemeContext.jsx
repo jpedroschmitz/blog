4f62cfe548c539833d10df5fab7ede2a49f83b6e
João Pedro Schmitz <jpedroschmitz@hotmail.com>
2019-06-10 14:50:27 -0300

    Add context for night mode

---
+++ src/context/ThemeContext.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+
+const defaultState = {
+  dark: false,
+  toggleDark: () => {},
+};
+
+const ThemeContext = React.createContext(defaultState);
+
+const supportsDarkMode = () => window.matchMedia("(prefers-color-scheme: dark)").matches === true;
+
+class ThemeProvider extends React.Component {
+  state = {
+    dark: false,
+  };
+
+  componentDidMount() {
+    if (localStorage.getItem("dark")) {
+      const lsDark = JSON.parse(localStorage.getItem("dark"));
+      if (lsDark) {
+        this.setState({ dark: lsDark });
+      } else if (supportsDarkMode()) {
+        this.setState({ dark: true });
+      }
+    } else {
+      const { dark } = this.state;
+      localStorage.setItem("dark", JSON.stringify(dark));
+    }
+  }
+
+  toggleDark = () => {
+    const { dark: darkEl } = this.state;
+    localStorage.setItem("dark", JSON.stringify(!darkEl));
+    this.setState({ dark: !darkEl });
+  };
+
+  render() {
+    const { children } = this.props;
+    const { dark } = this.state;
+    return (
+      <ThemeContext.Provider
+        value={{
+          dark,
+          toggleDark: this.toggleDark,
+        }}
+      >
+        {children}
+      </ThemeContext.Provider>
+    );
+  }
+}
+
+export default ThemeContext;
+
+export { ThemeProvider };
