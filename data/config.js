2e3ea24cf405591635e8a3ee95981ee8dced4c8b
João Pedro Schmitz <jpedroschmitz@hotmail.com>
2019-06-13 11:54:13 -0300

    Add basic info

---
+++ data/config.js
@@ -0,0 +1,29 @@
+const config = {
+  siteTitle: "João Pedro Schmitz",
+  headerTitle: "João Pedro S.",
+  siteTitleShort: "joaopedro.cc",
+  siteTitleAlt: "João Pedro Schmitz",
+  siteLogo: "/images/favicon.png",
+  siteBanner: "/images/general/banner.png",
+  siteUrl: "https://www.joaopedro.cc",
+  pathPrefix: "/",
+  siteDescription: "Um blog de um eterno estudante de desenvolvimento web!",
+  siteRss: "/rss.xml",
+  googleAnalyticsID: "UA-134411592-1",
+  disqusShortname: "blog-do-joao-pedro",
+  themeColor: "#00E2BC",
+  backgroundColor: "#FFFFFF",
+  copyright: "João Pedro Schmitz",
+};
+
+if (config.pathPrefix === "/") {
+  config.pathPrefix = "";
+} else {
+  config.pathPrefix = `/${config.pathPrefix.replace(/^\/|\/$/g, "")}`;
+}
+
+if (config.siteUrl.substr(-1) === "/") config.siteUrl = config.siteUrl.slice(0, -1);
+
+if (config.siteRss && config.siteRss[0] !== "/") config.siteRss = `/${config.siteRss}`;
+
+module.exports = config;
