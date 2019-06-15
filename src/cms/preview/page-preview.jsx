ff1a12e6dd807541f5ca155be40839bc275943d7
João Pedro Schmitz <jpedroschmitz@hotmail.com>
2019-06-15 14:12:50 -0300

    Fix preview styling

--- src/cms/preview/page-preview.jsx
+++ src/cms/preview/page-preview.jsx
@@ -1,10 +1,15 @@
 import React from "react";
+import { StyleSheetManager } from "styled-components";
 import Content from "../../components/Content";
 import InternalBar from "../../components/InternalBar";
 
-export default ({ entry, widgetFor }) => (
-  <>
-    <InternalBar title={entry.getIn(["title"])} introduction="Preview de Page" />
-    <Content html={widgetFor("body")} />
-  </>
-);
+export default ({ entry, widgetFor }) => {
+  const iframe = document.querySelector("#nc-root iframe");
+  const iframeHeadElem = iframe && iframe.contentDocument.head;
+  return (
+    <StyleSheetManager target={iframeHeadElem}>
+      <InternalBar title={entry.getIn(["title"])} introduction="Preview de Page" />
+      <Content html={widgetFor("body")} />
+    </StyleSheetManager>
+  );
+};
--- src/cms/preview/post-preview.jsx
+++ src/cms/preview/post-preview.jsx
@@ -1,8 +1,24 @@
 import React from "react";
+import moment from "moment";
+import { StyleSheetManager } from "styled-components";
 import Content from "../../components/Content";
+import ContentInfo from "../../components/ContentInfo";
 
-export default ({ widgetFor }) => (
-  <>
-    <Content html={widgetFor("body")} />
-  </>
-);
+export default ({ widgetFor }) => {
+  const iframe = document.querySelector("#nc-root iframe");
+  const iframeHeadElem = iframe && iframe.contentDocument.head;
+  return (
+    <StyleSheetManager target={iframeHeadElem}>
+      <ContentInfo
+        timeToRead="1"
+        title={widgetFor("body")}
+        date={moment(widgetFor("body"), "YYYYMMDD").fromNow()}
+        category={widgetFor("category")}
+        color="#000"
+        image={widgetFor("image")}
+        tags={widgetFor("tags")}
+      />
+      <Content html={widgetFor("body")} />
+    </StyleSheetManager>
+  );
+};
