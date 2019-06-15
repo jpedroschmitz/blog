8887b729d7868e975c46efdf727957225d41ecb3
João Pedro Schmitz <jpedroschmitz@hotmail.com>
2019-06-15 10:37:19 -0300

    Add max-width

--- src/components/UI/Grid/Container/S.Container.js
+++ src/components/UI/Grid/Container/S.Container.js
@@ -4,6 +4,7 @@ import breakpoints from "../../../../styles/breakpoints";
 export default styled.div`
   margin: 100px auto 100px auto;
   width: 75%;
+  max-width: 1200px;
 
   ${breakpoints.xlg} {
     width: 86%;
