c82f9e76463830d4f3360a7b15170f5ca387e4b7
João Pedro Schmitz <jpedroschmitz@hotmail.com>
2019-08-09 20:31:42 -0300

    chore: Add default theme

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { ThemeProvider } from 'styled-components';
 import 'minireset.css';
 import 'prismjs/themes/prism-okaidia.css';
 
-import { ThemeProvider } from './src/context/ThemeContext';
+import { StateProvider } from './src/context/StateContext';
+import defaultTheme from './src/styles/themes/default';
 
 export const wrapRootElement = ({ element }) => (
-  <ThemeProvider>{element}</ThemeProvider>
+  <StateProvider>
+    <ThemeProvider theme={defaultTheme}>{element}</ThemeProvider>
+  </StateProvider>
 );
