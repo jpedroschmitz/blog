d48991f7c652eec59cb46a8786c567d0f2d1d0db
João Pedro Schmitz <jpedroschmitz@hotmail.com>
2019-09-28 23:21:29 -0300

    chore: Remove preview from CMS

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,8 +24,8 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-netlify-cms',
       options: {
-        modulePath: `${__dirname}/src/cms/cms.js`,
         htmlTitle: 'Gerenciador de Conteúdo',
+        htmlFavicon: '/favicon.png',
       },
     },
     'gatsby-plugin-netlify',
